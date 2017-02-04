951654b818cb7c5eba3235fc1c39821fedf8135a
zertu <290055513@qq.com>
2017-02-04 22:26:18 +0800

    字体更换

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { LoginComponent } from './login/login.component'
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EditorComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/editor/editor.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { EditorComponent } from './editor.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        EditorComponent,
+    ],
+    exports: [
+        EditorComponent,
+    ]
+})
+export class EditorModule {
+
+}
