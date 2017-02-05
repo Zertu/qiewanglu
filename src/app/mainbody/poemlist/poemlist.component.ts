653ad6c8cc09a9263557500c5ed42560ab400883
zertu <290055513@qq.com>
2017-02-05 16:23:45 +0800

    数据获取服务

--- src/app/mainbody/poemlist/poemlist.component.ts
+++ src/app/mainbody/poemlist/poemlist.component.ts
@@ -1,15 +1,19 @@
+import { GetpoemsService } from './../getpoems.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-poemlist',
   templateUrl: './poemlist.component.html',
-  styleUrls: ['./poemlist.component.css']
+  styleUrls: ['./poemlist.component.css'],
+  providers:[GetpoemsService]
 })
 export class PoemlistComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private GetpoemsService : GetpoemsService
+  ) { }
 
   ngOnInit() {
   }
-
+  poemslist=this.GetpoemsService.getpoemlist()
 }
