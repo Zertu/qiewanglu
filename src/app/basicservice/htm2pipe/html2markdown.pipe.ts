1e28543147e67c802491c2dd110e23bcdbefda57
zertu <290055513@qq.com>
2017-02-05 11:33:46 +0800

    markdown管道

--- src/app/basicservice/htm2pipe/html2markdown.pipe.ts
+++ src/app/basicservice/htm2pipe/html2markdown.pipe.ts
@@ -1,13 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
-let markdown = require( "markdown" ).markdown;
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser'
+let markdown = require("markdown").markdown;
 
 @Pipe({
   name: 'html2markdown'
 })
-export class html2markdown implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return markdown.toHTML(value);
+export class html2markdown implements PipeTransform, SafeHtml {
+  constructor(private domSanitizer:DomSanitizer){}
+  transform(value: any, args?: any): SafeHtml {
+    return this.domSanitizer.bypassSecurityTrustHtml(markdown.toHTML(value))
   }
 
 }
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-
 @Component({
     moduleId: module.id,
     selector: 'editor',
     templateUrl: 'editor.component.html',
     styleUrls: ['editor.component.scss']
 })
-export class EditorComponent {
-
-    constructor(){}
-    poem:string=''
+export class EditorComponent  {
+    constructor() { }
+    poem: string = ''
 }
