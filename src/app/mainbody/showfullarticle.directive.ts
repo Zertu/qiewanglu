a495d94cb567f01d55b5cd314ae68771abcbc9f5
zertu <290055513@qq.com>
2017-02-07 14:25:50 +0800

    添加显示全部功能

--- src/app/mainbody/showfullarticle.directive.ts
+++ src/app/mainbody/showfullarticle.directive.ts
@@ -1,13 +1,15 @@
-import { Directive, ElementRef, Input, Renderer } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[Showfullarticle]'
 })
 export class ShowfullarticleDirective {
-
-  constructor(el: ElementRef, renderer: Renderer) {
-    console.log(el)
-    renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'yellow');
+  @HostListener('click') onclick() {
+    console.log(this)
+    this.showall()
   }
-
+  constructor(private el: ElementRef, private renderer: Renderer) { }
+  showall() {
+    this.renderer.setElementClass(this.el.nativeElement, 'showall', true)
+  }
 }
