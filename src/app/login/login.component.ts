1c8fcb27814ba42f4a9417bc5136956197cf145e
zertu <290055513@qq.com>
2017-02-08 14:17:59 +0800

    修改selector

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     moduleId: module.id,
-    selector: 'login',
+    selector: 'app-login',
     templateUrl: 'login.component.html',
     styleUrls: ['login.component.scss']
 })
