605bb9b91ef0e73832448b06e1b149707293999c
陶玉君 <Jader_tao@qq.com>
2017-02-08 21:38:28 +0800

    login样式与逻辑beta

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
+import {Router}            from '@angular/router'
+import {Title}             from '@angular/platform-browser'
+import { User }            from'./user';
+import {LoginService}      from './login.service'
+
 
 @Component({
     moduleId: module.id,
-    selector: 'login',
-    templateUrl: 'login.component.html',
-    styleUrls: ['login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    providers:[LoginService]
 })
-export class LoginComponent {
+export class LoginComponent  {
+
+    constructor(
+        private router: Router,
+        private titleService: Title,
+        private loginService: LoginService
+    ){}
+
+    public user = new User('','');
+
+
+    login(){
+        let result=this.loginService.login(this.user)
+        alert(result?'success':'failure');
+    }
+
+ /*   get userInfo() {
+            return JSON.stringify(this.user);
+        }
+  */ 
+  
+
 }
---
+++ src/app/login/mock-users.component.ts
@@ -0,0 +1,6 @@
+import { User } from'./user';
+
+export const USER : User[]=[
+    {email:'bot1@test.com',password:'testpwd123'},
+    {email:'bot2@temp.com',password:'123456pwd'},
+];
