753374b8555287fc5c828709a43b3a9e8e0b96df
苏克力 <JadeTao@users.noreply.github.com>
2017-02-08 22:28:13 +0800

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
+import { User }            from'./user';
+import {LoginService}      from './login.service'
+
 
 @Component({
     moduleId: module.id,
     selector: 'app-login',
-    templateUrl: 'login.component.html',
-    styleUrls: ['login.component.scss']
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    providers:[LoginService]
 })
-export class LoginComponent {
+export class LoginComponent  {
+
+    constructor(
+        private loginService: LoginService
+    ){}
+
+    public user = new User('','');
+
+
+    login(){
+        let result=this.loginService.login(this.user)
+        alert(result?'success':'failure');
+    }
+
+ /*   get userInfo() {
+            return JSON.stringify(this.user);
+        }
+  */ 
+  
+
 }
